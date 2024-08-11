89aa38c887a94c19217406ce129d9c3a6d2da6a6
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-08-12 00:58:13 +0200

    db context added

---
+++ src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Infrastructure/Ef/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Skillup.Modules.Auth.Infrastructure.Ef
+{
+    internal class AuthDbContext(DbContextOptions<AuthDbContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("auth");
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
