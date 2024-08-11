a354fc3cd42293ca44726e79aa3cbc22db9f50a1
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-08-12 00:06:14 +0200

    postgres added

---
+++ src/backend/Skillup/Shared/Skillup.Shared.Infrastructure/Postgres/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Skillup.Shared.Infrastructure.EnvironmentInjector;
+
+namespace Skillup.Shared.Infrastructure.Postgres
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddPostgres<T>(this IServiceCollection services) where T : DbContext
+        {
+            var connectionStringTemplate = services.GetSection("postgres:connectionString").Value;
+            PostgresOptions options = (PostgresOptions)new PostgresOptions(connectionStringTemplate).InjectEnvironment();
+
+            services.AddSingleton(options);
+            services.AddDbContext<T>(x => x.UseNpgsql(options.ConnectionString));
+            return services;
+        }
+    }
+}
---
+++ src/backend/Skillup/Shared/Skillup.Shared.Infrastructure/Postgres/PostgresOptions.cs
@@ -0,0 +1,25 @@
+﻿using Skillup.Shared.Abstractions.Options;
+using Skillup.Shared.Infrastructure.EnvironmentInjector;
+
+namespace Skillup.Shared.Infrastructure.Postgres
+{
+    public class PostgresOptions(string connectionStringTemplate) : IOption
+    {
+        [EnvironmentVariable("POSTGRES_HOST")]
+        public string Host { get; set; } = default!;
+
+        [EnvironmentVariable("POSTGRES_DB")]
+        public string DatabaseName { get; set; } = default!;
+
+        [EnvironmentVariable("POSTGRES_USER")]
+        public string User { get; set; } = default!;
+
+        [EnvironmentVariable("POSTGRES_PASSWORD")]
+        public string Password { get; set; } = default!;
+
+        [EnvironmentVariable("POSTGRES_PORT")]
+        public int Port { get; set; } = default!;
+
+        public string ConnectionString { get => connectionStringTemplate; }
+    }
+}
