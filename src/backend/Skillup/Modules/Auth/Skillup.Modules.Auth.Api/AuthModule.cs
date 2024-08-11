3f007360c09aef0af0ada55dda119e663b32adb1
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-08-12 00:47:06 +0200

    auth module with onion architecture added

---
+++ src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Api/AuthModule.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Skillup.Modules.Auth.Application;
+using Skillup.Modules.Auth.Core;
+using Skillup.Modules.Auth.Infrastructure;
+using Skillup.Shared.Abstractions.Modules;
+
+namespace Skillup.Modules.Auth.Api
+{
+    internal class AuthModule : IModule
+    {
+        public string Name => "Auth";
+
+        public void Register(IServiceCollection services)
+        {
+            services.AddCore();
+            services.AddApplication();
+            services.AddInfrastructure();
+        }
+
+        public void Use(IApplicationBuilder app)
+        {
+        }
+    }
+}
