6ae3590161f9c6f47bfcfbfeba335429f60d362b
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-01 20:22:55 +0200

    added sending an email with activation code

---
+++ src/backend/Skillup/Modules/Mails/Skillup.Modules.Mails.Core/Templates/AccountActivation/AccountActivationTemplate.cs
@@ -0,0 +1,14 @@
+﻿namespace Skillup.Modules.Mails.Core.Templates.AccountActivation
+{
+    internal class AccountActivationTemplate(int code) : ITemplate
+    {
+        private class AccountActivationModel(int code) : TemplateModel
+        {
+            public int Code { get; private set; } = code;
+        }
+
+        public string Subject { get; } = "Account Activation";
+        public string Path { get; } = @"AccountActivation/account_activation.html";
+        public TemplateModel Model { get; } = new AccountActivationModel(code);
+    }
+}
