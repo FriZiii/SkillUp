3960766cca4e57fcad37efe85c92195e597edadd
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-01 20:24:31 +0200

    new exceptions have been added

---
+++ src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Core/Exceptions/InvalidCredentialsException.cs
@@ -0,0 +1,11 @@
+﻿using Skillup.Shared.Abstractions.Exceptions;
+
+namespace Skillup.Modules.Auth.Core.Exceptions
+{
+    internal class InvalidCredentialsException : SkillupException
+    {
+        public InvalidCredentialsException() : base("Invalid credentials.")
+        {
+        }
+    }
+}
---
+++ src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Core/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using Skillup.Shared.Abstractions.Exceptions;
+
+namespace Skillup.Modules.Auth.Core.Exceptions
+{
+    internal class UserNotFoundException : SkillupException
+    {
+        public string Email { get; }
+        public Guid UserId { get; }
+
+        public UserNotFoundException(Guid userId) : base($"User with ID: '{userId}' was not found.")
+        {
+            UserId = userId;
+        }
+
+        public UserNotFoundException(string email) : base($"User with email: '{email}' was not found.")
+        {
+            Email = email;
+        }
+    }
+}
