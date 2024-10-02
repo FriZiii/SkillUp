8c782a81c382932973e0408c4d425c818d6b8b76
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-02 13:08:01 +0200

    Update ChangePasswordHandler.cs

--- src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Core/Commands/Handlers/ChangePasswordHandler.cs
+++ src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Core/Commands/Handlers/ChangePasswordHandler.cs
@@ -1,12 +1,30 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Skillup.Modules.Auth.Core.Entities;
+using Skillup.Modules.Auth.Core.Exceptions;
+using Skillup.Modules.Auth.Core.Repositories;
 
 namespace Skillup.Modules.Auth.Core.Commands.Handlers
 {
-    internal class ChangePasswordHandler : IRequestHandler<ChangePassword>
+    internal class ChangePasswordHandler(IUserRepository userRepository, IPasswordHasher<User> passwordHasher) : IRequestHandler<ChangePassword>
     {
-        public Task Handle(ChangePassword request, CancellationToken cancellationToken)
+        private readonly IUserRepository _userRepository = userRepository;
+        private readonly IPasswordHasher<User> _passwordHasher = passwordHasher;
+
+        public async Task Handle(ChangePassword request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var user = await _userRepository.Get(request.UserId) ?? throw new UserNotFoundException(request.UserId);
+
+            if (_passwordHasher.VerifyHashedPassword(user, user.Password, request.CurrentPassword) ==
+                PasswordVerificationResult.Failed)
+            {
+                throw new InvalidPasswordException("current password is invalid");
+            }
+
+            user.Password = _passwordHasher.HashPassword(user, request.NewPassword); ;
+            await _userRepository.Update(user);
+
+            //TODO : LOGS
         }
     }
 }
