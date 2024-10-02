eec83aff257842c1b83d29fd332f421a3f389c2c
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-02 13:07:55 +0200

    Update SignInHandler.cs

--- src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Core/Commands/Handlers/SignInHandler.cs
+++ src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Core/Commands/Handlers/SignInHandler.cs
@@ -1,12 +1,43 @@
-﻿using MediatR;
+﻿using MassTransit;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Skillup.Modules.Auth.Core.Entities;
+using Skillup.Modules.Auth.Core.Exceptions;
+using Skillup.Modules.Auth.Core.Repositories;
+using Skillup.Shared.Abstractions.Time;
 
 namespace Skillup.Modules.Auth.Core.Commands.Handlers
 {
-    internal class SignInHandler : IRequestHandler<SignIn>
+    internal class SignInHandler(IUserRepository userRepository,
+        RegistrationOptions registrationOptions,
+        IPublishEndpoint publishEndpoint,
+        IPasswordHasher<User> passwordHasher,
+        IClock clock) : IRequestHandler<SignIn>
     {
-        public Task Handle(SignIn request, CancellationToken cancellationToken)
+        private readonly IUserRepository _userRepository = userRepository;
+        private readonly RegistrationOptions _registrationOptions = registrationOptions;
+        private readonly IPublishEndpoint _publishEndpoint = publishEndpoint;
+        private readonly IPasswordHasher<User> _passwordHasher = passwordHasher;
+        private readonly IClock _clock = clock;
+
+        public async Task Handle(SignIn request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var user = await _userRepository.Get(request.Email.ToLowerInvariant()) ?? throw new InvalidCredentialsException();
+
+            if (user.State != UserState.Active)
+            {
+                throw new UserNotActiveException(user.Id);
+            }
+
+            if (_passwordHasher.VerifyHashedPassword(user, user.Password, request.Password) ==
+                PasswordVerificationResult.Failed)
+            {
+                throw new InvalidCredentialsException();
+            }
+
+            //TODO : JWT
+
+            //TODO : LOGS
         }
     }
 }
