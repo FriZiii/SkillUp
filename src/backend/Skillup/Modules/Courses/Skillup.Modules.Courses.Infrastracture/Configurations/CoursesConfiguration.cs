43ec1ae96e4e06c6682ce0dd6751fddc9a227122
MadziaSzmatloch <98941475+MadziaSzmatloch@users.noreply.github.com>
2024-10-04 12:55:53 +0200

    course configuration refactor

---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Infrastracture/Configurations/CoursesConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Skillup.Modules.Courses.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillup.Modules.Courses.Infrastracture.Configurations
+{
+    public class CoursesConfiguration : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.ToTable("Courses");
+            builder.OwnsOne(c => c.Info);
+            builder.HasOne(c => c.Category)
+                .WithMany(category => category.Courses)
+                .HasForeignKey(c => c.Id);
+            builder.HasOne(c => c.Subcategory)
+                .WithMany(s => s.Courses)
+                .HasForeignKey(c => c.Id);
+            builder.Property(c => c.Level)
+                .HasConversion<string>();
+        }
+    }
+}
--- src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Infrastracture/CoursesDbContext.cs
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Infrastracture/CoursesDbContext.cs
@@ -1,29 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Skillup.Modules.Courses.Core.Entities;
+using Skillup.Modules.Courses.Infrastracture.Configurations;
 
 namespace Skillup.Modules.Courses.Infrastracture
 {
     internal class CoursesDbContext(DbContextOptions<CoursesDbContext> options) : DbContext(options)
     {
         public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Subcategory> Subcategories { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasDefaultSchema("courses");
             modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
 
-            modelBuilder.Entity<Course>()
-                .OwnsOne(c => c.Info);
-            modelBuilder.Entity<Course>()
-                .HasOne(c => c.Category)
-                .WithMany(category => category.Courses)
-                .HasForeignKey(c => c.Id);
-            modelBuilder.Entity<Course>()
-                .HasOne(c => c.Subcategory)
-                .WithMany(s => s.Courses)
-                .HasForeignKey(c => c.Id);
-            modelBuilder.Entity<Course>()
-                .Property(c => c.Level)
-                .HasConversion<string>();
+            modelBuilder.ApplyConfiguration(new CoursesConfiguration());
         }
     }
 }
