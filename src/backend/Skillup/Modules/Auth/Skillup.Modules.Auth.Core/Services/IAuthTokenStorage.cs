9859118757a6b5b42542bb7b56ea13399912ac8a
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-07 01:04:53 +0200

    added in memory storage

---
+++ src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Core/Services/IAuthTokenStorage.cs
@@ -0,0 +1,10 @@
+﻿using Skillup.Shared.Abstractions.Auth;
+
+namespace Skillup.Modules.Auth.Core.Services
+{
+    internal interface IAuthTokenStorage
+    {
+        void SetToken(Guid requestId, AuthTokens tokens);
+        AuthTokens GetTokens(Guid requestId);
+    }
+}
