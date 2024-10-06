7e84e7c68e80fc406bf182143fe695e9286eebdb
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-07 01:06:10 +0200

    Create TokenController.cs

---
+++ src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Api/Controllers/TokenController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Skillup.Modules.Auth.Api.Controllers.Base;
+using IMediator = MediatR.IMediator;
+
+namespace Skillup.Modules.Auth.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    internal class TokenController : BaseController
+    {
+        private readonly IMediator _mediator;
+
+        public TokenController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("refresh")]
+        public async Task<IActionResult> RefreshToken()
+        {
+            var result = await _mediator.Send(null);
+            return Ok(result);
+        }
+    }
+}
