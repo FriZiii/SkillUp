786af3b21ce1ecf68b778514acdce8041dbb3af3
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-07 01:05:55 +0200

    Update UserController.cs

--- src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Api/Controllers/UserController.cs
+++ src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Api/Controllers/UserController.cs
@@ -1,7 +1,10 @@
-﻿using Skillup.Modules.Auth.Api.Controllers.Base;
+﻿using Microsoft.AspNetCore.Mvc;
+using Skillup.Modules.Auth.Api.Controllers.Base;
 
 namespace Skillup.Modules.Auth.Api.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     internal class UserController : BaseController
     {
     }
