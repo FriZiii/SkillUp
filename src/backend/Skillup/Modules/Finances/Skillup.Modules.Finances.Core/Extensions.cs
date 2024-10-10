f3ba752788f2748300e0a3238a1032a5ec037880
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-10 12:27:01 +0200

    renamed module and added basic setup

---
+++ src/backend/Skillup/Modules/Finances/Skillup.Modules.Finances.Core/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Skillup.Modules.Finances.Api")]
+namespace Skillup.Modules.Finances.Core
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
