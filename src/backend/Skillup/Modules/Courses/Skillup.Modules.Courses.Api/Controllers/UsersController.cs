561bdf71f05fbaf42a8b5c6f92dc8492453910ef
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-11 15:31:10 +0200

    Create UsersController.cs

---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Api/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using MassTransit.Mediator;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Skillup.Modules.Courses.Core.Requests;
+
+namespace Skillup.Modules.Courses.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    internal class UsersController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpPut("{UserId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> EditUser(EditUserRequest request)
+        {
+            request.UserId = request.UserId;
+            await _mediator.Send(request);
+            return Ok(request);
+        }
+
+        [HttpPut("{UserId}/Privacy-Settings")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> EditPrivacySettings(EditUserPrivacySettingsRequest request)
+        {
+            request.UserId = request.UserId;
+            await _mediator.Send(request);
+            return Ok(request);
+        }
+
+        [HttpPut("{UserId}/Profile-Picture")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> EditProfilePicture()
+        {
+            //TODO : EditProfilePicture
+            return Ok();
+        }
+    }
+}
