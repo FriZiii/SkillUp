c707d2dc4581c54c00c7d4e7e3b06de794889841
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-11 14:51:57 +0200

    added consumer

---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Infrastracture/Consumers/SignedUpConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using MediatR;
+using Skillup.Modules.Courses.Core.Requests;
+using Skillup.Shared.Abstractions.Events.Auth;
+
+namespace Skillup.Modules.Courses.Infrastracture.Consumers
+{
+    internal class SignedUpConsumer(IMediator mediator) : IConsumer<SignedUp>
+    {
+        private readonly IMediator _mediator = mediator;
+
+        public async Task Consume(ConsumeContext<SignedUp> context)
+        {
+            await _mediator.Send(new AddUserRequest(context.Message.UserId, context.Message.Email));
+        }
+    }
+}
