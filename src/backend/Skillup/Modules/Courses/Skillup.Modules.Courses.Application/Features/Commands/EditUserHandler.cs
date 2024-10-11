aefdb2712aea97138ec2f05904f36ea21bd00411
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-11 16:45:37 +0200

    Added EditUser feature

---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Application/Features/Commands/EditUserHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Skillup.Modules.Courses.Core.Entities.UserEntities;
+using Skillup.Modules.Courses.Core.Interfaces;
+using Skillup.Modules.Courses.Core.Requests;
+
+namespace Skillup.Modules.Courses.Application.Features.Commands
+{
+    internal class EditUserHandler : IRequestHandler<EditUserRequest>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public EditUserHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task Handle(EditUserRequest request, CancellationToken cancellationToken)
+        {
+            var user = new User()
+            {
+                Id = request.UserId,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Details = new UserDetails()
+                {
+                    Biography = request.Biography,
+                    Title = request.Title,
+                },
+                SocialMediaLinks = request.SocialMediaLinks,
+            };
+
+            await _userRepository.Edit(user);
+        }
+    }
+}
---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Core/Requests/EditUserRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Skillup.Modules.Courses.Core.DTO;
+using System.Text.Json.Serialization;
+
+namespace Skillup.Modules.Courses.Core.Requests
+{
+    public record EditUserRequest : UserDto, IRequest
+    {
+        [JsonIgnore]
+        public Guid UserId { get; set; }
+    };
+}
