bf354538f83a64fd103720c94edcb56b1bd8469e
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-11 16:45:56 +0200

    Create UserDto.cs

---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Core/DTO/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using Skillup.Modules.Courses.Core.Entities.UserEntities;
+using Skillup.Shared.Abstractions.Kernel.ValueObjects;
+
+namespace Skillup.Modules.Courses.Core.DTO
+{
+    public record UserDto
+    {
+        public Guid Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required Email Email { get; set; }
+
+        public string Title { get; set; }
+        public string Biography { get; set; }
+
+        public SocialMediaLinks SocialMediaLinks { get; set; }
+    }
+}
