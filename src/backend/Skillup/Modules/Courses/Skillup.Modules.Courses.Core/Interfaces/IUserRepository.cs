cb8bff4b59be53e73a171d2b4ebdf82655ab6e5b
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-11 16:46:41 +0200

    added user repository

---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Skillup.Modules.Courses.Core.Entities.UserEntities;
+
+namespace Skillup.Modules.Courses.Core.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task Add(User user);
+        Task Edit(User user);
+        Task EditUserPrivacySettings(Guid userId, PrivacySettings privacySettings);
+    }
+}
---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Infrastracture/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skillup.Modules.Courses.Core.Entities.UserEntities;
+using Skillup.Modules.Courses.Core.Interfaces;
+
+namespace Skillup.Modules.Courses.Infrastracture.Repositories
+{
+    internal class UserRepository : IUserRepository
+    {
+        private readonly CoursesDbContext _context;
+        private readonly DbSet<User> _users;
+
+        public UserRepository(CoursesDbContext context)
+        {
+            _context = context;
+            _users = _context.Users;
+        }
+
+        public async Task Add(User user)
+        {
+            await _users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Edit(User user)
+        {
+            var userToEdit = await _users.FirstOrDefaultAsync(x => x.Id == user.Id) ?? throw new Exception(); //TODO: custom exception
+
+            userToEdit.Email = user.Email;
+            userToEdit.FirstName = user.FirstName;
+            userToEdit.LastName = user.LastName;
+            userToEdit.Details = user.Details;
+            userToEdit.SocialMediaLinks = user.SocialMediaLinks;
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EditUserPrivacySettings(Guid userId, PrivacySettings privacySettings)
+        {
+            var userToEdit = await _users.FirstOrDefaultAsync(x => x.Id == userId) ?? throw new Exception(); //TODO: custom exception
+
+            userToEdit.PrivacySettings = privacySettings;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
