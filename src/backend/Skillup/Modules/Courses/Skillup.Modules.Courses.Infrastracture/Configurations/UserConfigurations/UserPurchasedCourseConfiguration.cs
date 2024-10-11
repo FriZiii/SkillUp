a88713db73d7c9cde08fbfb7eba0c4531b05cb83
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-11 16:46:48 +0200

    Create UserPurchasedCourseConfiguration.cs

---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Infrastracture/Configurations/UserConfigurations/UserPurchasedCourseConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Skillup.Modules.Courses.Core.Entities.CourseEntities;
+using Skillup.Modules.Courses.Core.Entities.UserEntities;
+
+namespace Skillup.Modules.Courses.Infrastracture.Configurations.UserConfigurations
+{
+    internal class UserPurchasedCourseConfiguration : IEntityTypeConfiguration<UserPurchasedCourse>
+    {
+        public void Configure(EntityTypeBuilder<UserPurchasedCourse> builder)
+        {
+            builder.HasKey(pc => new { pc.UserId, pc.CourseId });
+
+            builder.HasOne<User>()
+                .WithMany(u => u.PurchasedCourses)
+                .HasForeignKey(pc => pc.UserId);
+
+            builder.HasOne<Course>()
+                .WithMany()
+                .HasForeignKey(pc => pc.CourseId);
+
+            builder.HasIndex(pc => new { pc.UserId, pc.CourseId })
+                .IsUnique();
+        }
+    }
+}
