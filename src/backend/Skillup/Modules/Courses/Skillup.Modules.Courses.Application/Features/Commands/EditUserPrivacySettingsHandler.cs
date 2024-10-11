5b9f6d750322bb950f4cd6f8d964df2d97ac5625
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-11 16:45:53 +0200

    Added EditUserPrivacySettings feature

---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Application/Features/Commands/EditUserPrivacySettingsHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Skillup.Modules.Courses.Core.Entities.UserEntities;
+using Skillup.Modules.Courses.Core.Interfaces;
+using Skillup.Modules.Courses.Core.Requests;
+
+namespace Skillup.Modules.Courses.Application.Features.Commands
+{
+    internal class EditUserPrivacySettingsHandler : IRequestHandler<EditUserPrivacySettingsRequest>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public EditUserPrivacySettingsHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task Handle(EditUserPrivacySettingsRequest request, CancellationToken cancellationToken)
+        {
+            var privacySettings = new PrivacySettings()
+            {
+                ShowCoursesOnUserProfile = request.ShowCoursesOnUserProfile,
+                IsAccountPublicForLoggedInUsers = request.IsAccountPublicForLoggedInUsers,
+            };
+
+            await _userRepository.EditUserPrivacySettings(request.UserId, privacySettings);
+        }
+    }
+}
---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Core/Requests/EditUserPrivacySettingsRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System.Text.Json.Serialization;
+
+namespace Skillup.Modules.Courses.Core.Requests
+{
+    public record class EditUserPrivacySettingsRequest(bool IsAccountPublicForLoggedInUsers, bool ShowCoursesOnUserProfile) : IRequest
+    {
+        [JsonIgnore]
+        public Guid UserId { get; set; }
+    }
+}
