61903667983b73010e2fdd46fb8874860ae66194
MadziaSzmatloch <magdaszmatloch@gmail.com>
2024-10-11 15:22:27 +0200

    migration added

---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Infrastracture/Migrations/20241011131904_Thumbnail_Required.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Skillup.Modules.Courses.Infrastracture.Migrations
+{
+    /// <inheritdoc />
+    public partial class Thumbnail_Required : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ThumbnailUrl",
+                schema: "courses",
+                table: "Courses",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ThumbnailUrl",
+                schema: "courses",
+                table: "Courses",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
