0a9f1735e40a184c106f2958292932fa1e23a21c
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-13 01:27:03 +0200

    added methods to change user roles and states

--- src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Core/DAL/Repositories/UserRepository.cs
+++ src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Core/DAL/Repositories/UserRepository.cs
@@ -2,6 +2,7 @@
 using Skillup.Modules.Auth.Core.Entities;
 using Skillup.Modules.Auth.Core.Exceptions;
 using Skillup.Modules.Auth.Core.Repositories;
+using Skillup.Shared.Abstractions.Auth;
 
 namespace Skillup.Modules.Auth.Core.DAL.Repositories
 {
@@ -39,6 +40,19 @@ namespace Skillup.Modules.Auth.Core.DAL.Repositories
             var userToChange = await _users.FirstOrDefaultAsync(x => x.Id.Equals(userId)) ?? throw new UserNotFoundException(userId);
             userToChange.State = state;
             await _context.SaveChangesAsync();
+        }
+
+        public async Task ChangeRole(Guid userId, UserRole role)
+        {
+            var userToChange = await _users.FirstOrDefaultAsync(x => x.Id.Equals(userId)) ?? throw new UserNotFoundException(userId);
+            userToChange.Role = role;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<UserRole> GetUserRole(Guid userId)
+        {
+            var user = await _users.FirstOrDefaultAsync(x => x.Equals(userId)) ?? throw new UserNotFoundException(userId);
+            return user.Role;
         }
     }
 }
