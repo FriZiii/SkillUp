2031058298e2263cffb6a551d3d2839fe59cd183
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-13 11:29:03 +0200

    fixes

--- src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Core/Features/Handlers/Password/ChangePasswordHandler.cs
+++ src/backend/Skillup/Modules/Auth/Skillup.Modules.Auth.Core/Features/Handlers/Password/ChangePasswordHandler.cs
@@ -1,15 +1,14 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Identity;
-using Skillup.Modules.Auth.Core.Entities;
 using Skillup.Modules.Auth.Core.Features.Requests.Password;
 using Skillup.Modules.Auth.Core.Repositories;
 
 namespace Skillup.Modules.Auth.Core.Features.Handlers.Password
 {
-    internal class ChangePasswordHandler(IUserRepository userRepository, IPasswordHasher<User> passwordHasher) : IRequestHandler<ChangePasswordRequest>
+    internal class ChangePasswordHandler(IUserRepository userRepository, IPasswordHasher<Entities.User> passwordHasher) : IRequestHandler<ChangePasswordRequest>
     {
         private readonly IUserRepository _userRepository = userRepository;
-        private readonly IPasswordHasher<User> _passwordHasher = passwordHasher;
+        private readonly IPasswordHasher<Entities.User> _passwordHasher = passwordHasher;
 
         public async Task Handle(ChangePasswordRequest request, CancellationToken cancellationToken)
         {
