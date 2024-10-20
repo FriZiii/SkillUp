3d6c47f10fc20f902fc09c0c0bbd2c7c50fa1d61
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-20 12:49:20 +0200

    added primeng and tailwind

--- src/frontend/Skillup/src/app/app.component.ts
+++ src/frontend/Skillup/src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+import { definePreset } from 'primeng/themes';
+import { Aura } from 'primeng/themes/aura';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Skillup';
+
+  MyPreset = definePreset(Aura, {
+    semantic: {
+      primary: {
+        50: '{yellow.0}',
+        100: '{yellow.50}',
+        200: '{yellow.100}',
+        300: '{yellow.200}',
+        400: '{yellow.300}',
+        500: '{yellow.400}',
+        600: '{yellow.500}',
+        700: '{yellow.600}',
+        800: '{yellow.700}',
+        900: '{yellow.800}',
+        950: '{yellow.900}',
+      },
+    },
+  });
+
+  constructor(private config: PrimeNGConfig) {
+    this.config.theme.set({
+      preset: this.MyPreset,
+      options: {
+        cssLayer: {
+          name: 'primeng',
+          order: 'tailwind-base, tailwind-utilities, primeng',
+        },
+      },
+    });
+  }
 }
---
+++ src/frontend/Skillup/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: "selector",
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-primeui")],
+};
