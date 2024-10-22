911534e99062a438ad85a7c8b1a8734b343749b7
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-22 12:06:17 +0200

    added add course route

--- src/frontend/Skillup/src/app/app.routes.ts
+++ src/frontend/Skillup/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { SignUpComponent } from './auth/components/sign-up/sign-up.component';
 import { SignInComponent } from './auth/components/sign-in/sign-in.component';
 import { AccountActivationComponent } from './auth/components/account-activation/account-activation.component';
+import { AddCourseComponent } from './course/components/add-course/add-course.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'sign-up',
     component: SignUpComponent,
+  },
+  {
+    path: 'course/add',
+    component: AddCourseComponent,
   },
   {
     path: 'account-activation',
