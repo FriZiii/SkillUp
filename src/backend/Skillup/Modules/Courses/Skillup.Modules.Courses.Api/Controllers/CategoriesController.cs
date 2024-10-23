0e2af6717327543617ab26ba50b777a43fbf3a41
MadziaSzmatloch <98941475+MadziaSzmatloch@users.noreply.github.com>
2024-10-23 10:27:28 +0200

    logging in courses

--- src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Api/Controllers/CategoriesController.cs
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Api/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Skillup.Modules.Courses.Core.Requests.Queries;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -8,9 +9,10 @@ namespace Skillup.Modules.Courses.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    internal class CategoriesController(IMediator mediator) : ControllerBase
+    internal class CategoriesController(IMediator mediator, ILogger<CategoriesController> logger) : ControllerBase
     {
         private readonly IMediator _mediator = mediator;
+        private readonly ILogger<CategoriesController> _logger = logger;
 
         [HttpGet]
         [SwaggerOperation("Get categories")]
@@ -18,6 +20,9 @@ namespace Skillup.Modules.Courses.Api.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetAll()
         {
+
+            _logger.LogInformation("Pobieranie kategorii");
+            _logger.LogDebug("Loguje Debug");
             var courses = await _mediator.Send(new GetCategoriesRequest());
             return Ok(courses);
         }
--- src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Application/Features/Queries/GetCoursesHandler.cs
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Application/Features/Queries/GetCoursesHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using Skillup.Modules.Courses.Application.Mappings;
 using Skillup.Modules.Courses.Application.Operations;
 using Skillup.Modules.Courses.Core.Interfaces;
@@ -9,14 +10,17 @@ namespace Skillup.Modules.Courses.Application.Features.Queries
     public class GetCoursesHandler : IRequestHandler<GetCoursesRequest, IEnumerable<CourseDto>>
     {
         private readonly ICourseRepository _courseRepository;
+        private readonly ILogger<GetCoursesHandler> _logger;
 
-        public GetCoursesHandler(ICourseRepository courseRepository)
+        public GetCoursesHandler(ICourseRepository courseRepository, ILogger<GetCoursesHandler> logger)
         {
             _courseRepository = courseRepository;
+            _logger = logger;
         }
 
         public async Task<IEnumerable<CourseDto>> Handle(GetCoursesRequest request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Get Course handler");
             var mapper = new CourseMapper();
             var courses = await _courseRepository.GetAll();
             var coursesDtos = courses.Select(mapper.CourseToCourseDto).ToList();
