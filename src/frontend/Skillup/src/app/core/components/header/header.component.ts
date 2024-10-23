0a92019596ea47f6d8891ecd4110f130e3bb86aa
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-23 02:15:19 +0200

    header styling

--- src/frontend/Skillup/src/app/core/components/header/header.component.ts
+++ src/frontend/Skillup/src/app/core/components/header/header.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Drawer, DrawerModule } from 'primeng/drawer';
 import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { StyleClassModule } from 'primeng/styleclass';
+import { LogoComponent } from '../../../utils/components/logo/logo.component';
+import { FluidModule } from 'primeng/fluid';
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
+import { PopoverModule } from 'primeng/popover';
+import { BadgeModule } from 'primeng/badge';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonModule, RouterLink],
+  imports: [
+    BadgeModule,
+    OverlayBadgeModule,
+    PopoverModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    LogoComponent,
+    FluidModule,
+    ButtonModule,
+    RouterLink,
+    DrawerModule,
+    ButtonModule,
+    RippleModule,
+    AvatarModule,
+    StyleClassModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @ViewChild('drawerRef') drawerRef!: Drawer;
+
+  closeCallback(e: Event): void {
+    this.drawerRef.close(e);
+  }
+
+  visible: boolean = false;
+}
