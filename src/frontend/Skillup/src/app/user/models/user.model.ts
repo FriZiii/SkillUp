3a5bc70bbee828b8b0ed46c44d60b2c3e0597cb2
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-24 22:08:27 +0200

    user model update

--- src/frontend/Skillup/src/app/user/models/user.model.ts
+++ src/frontend/Skillup/src/app/user/models/user.model.ts
@@ -3,6 +3,9 @@ import { UserRole } from './user-role.model';
 export class User {
   id: string;
   role: UserRole;
+  firstName: string | null = null;
+  lastName: string | null = null;
+  profilePicture: string | null = null;
 
   constructor(id: string, role: UserRole) {
     this.id = id;
--- src/frontend/Skillup/src/app/user/services/user.service.ts
+++ src/frontend/Skillup/src/app/user/services/user.service.ts
@@ -1,8 +1,10 @@
 import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '../models/user.model';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { jwtDecode, JwtPayload } from 'jwt-decode';
 import { UserRole } from '../models/user-role.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 interface CustomJwtPayload extends JwtPayload {
   'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'?: string;
@@ -12,6 +14,7 @@ interface CustomJwtPayload extends JwtPayload {
   providedIn: 'root',
 })
 export class UserService {
+  httpClient = inject(HttpClient);
   private userSubject = new BehaviorSubject<User | null>(null);
 
   get user(): Observable<User | null> {
@@ -21,19 +24,28 @@ export class UserService {
   setUserFromToken(token: string): void {
     const decodedToken = jwtDecode<CustomJwtPayload>(token);
 
-    const user: User = {
-      id: decodedToken.sub!,
-      role: UserRole[
-        decodedToken[
-          'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'
-        ]! as keyof typeof UserRole
-      ],
-    };
+    const user = new User(
+      decodedToken.sub!,
+      decodedToken[
+        'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'
+      ]! as UserRole
+    );
 
-    this.userSubject.next(user);
+    this.getData(user.id, false).subscribe((response: any) => {
+      user.firstName = response.firstName;
+      user.lastName = response.lastName;
+      user.profilePicture = response.profilePicture;
+      this.userSubject.next(user);
+    });
   }
 
   clearUser(): void {
     this.userSubject.next(null);
+  }
+
+  private getData(userId: string, details: boolean) {
+    return this.httpClient.get(
+      `${environment.apiUrl}/courses/users/${userId}?details=${details}`
+    );
   }
 }
