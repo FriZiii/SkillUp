3b71faa869b0b24a3c53d8d93b8399b078ac6071
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-25 00:06:25 +0200

    removed unnecesery code

--- src/frontend/Skillup/src/app/course/services/category.service.ts
+++ src/frontend/Skillup/src/app/course/services/category.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { Category } from '../models/category.model';
-import { BehaviorSubject, catchError, delay, Observable, tap, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, Observable, tap, throwError } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
@@ -10,24 +10,27 @@ export class CategoryService {
   public categories = signal<Category[]>([]);
 
   private categoriesSubject = new BehaviorSubject<Category[]>([]);
-  public categories$: Observable<Category[]> = this.categoriesSubject.asObservable();
+  public categories$: Observable<Category[]> =
+    this.categoriesSubject.asObservable();
 
   constructor() {
     this.fetchCategories();
     this.categories$.subscribe((data) => {
       this.categories.set(data);
-    }); 
+    });
   }
 
   private fetchCategories() {
     this.httpClient
       .get<Category[]>(environment.apiUrl + '/Courses/Categories')
       .pipe(
-        delay(3000),
         tap((categories) => {
           this.categoriesSubject.next(categories);
         }),
-        catchError(error => {return throwError(() => error)}))
+        catchError((error) => {
+          return throwError(() => error);
+        })
+      )
       .subscribe();
   }
 }
