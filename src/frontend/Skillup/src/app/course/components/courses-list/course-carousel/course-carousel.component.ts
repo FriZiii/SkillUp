5d9104e44175d396345dde1dc3eb4fc22525cdfa
MadziaSzmatloch <98941475+MadziaSzmatloch@users.noreply.github.com>
2024-10-24 11:29:52 +0200

    courses style adjustments

--- src/frontend/Skillup/src/app/course/components/courses-list/course-carousel/course-carousel.component.ts
+++ src/frontend/Skillup/src/app/course/components/courses-list/course-carousel/course-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { CourseItemComponent } from "../course-item/course-item.component";
 import { CourseListItem } from '../../../models/course.model';
 import { CarouselModule } from 'primeng/carousel';
@@ -8,8 +8,25 @@ import { CarouselModule } from 'primeng/carousel';
   standalone: true,
   imports: [CourseItemComponent, CarouselModule],
   templateUrl: './course-carousel.component.html',
-  styles: []
+  styleUrl: './course-carousel.component.css',
 })
-export class CourseCarouselComponent {
+export class CourseCarouselComponent implements OnInit {
+  
   courses = input.required<CourseListItem[]>();
+  responsiveOptions: any[] | undefined;
+
+  ngOnInit(): void {
+    this.responsiveOptions = [
+      {
+          breakpoint: '1100px',
+          numVisible: 2,
+          numScroll: 1
+      },
+      {
+          breakpoint: '767px',
+          numVisible: 1,
+          numScroll: 1
+      }
+  ];
+  }
 }
