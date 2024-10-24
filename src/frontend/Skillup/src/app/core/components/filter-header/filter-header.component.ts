a3bbed54ff4e2d834e2d8a93d374d2a4556b477d
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-24 18:07:09 +0200

    auto hide has been implemented

--- src/frontend/Skillup/src/app/core/components/filter-header/filter-header.component.ts
+++ src/frontend/Skillup/src/app/core/components/filter-header/filter-header.component.ts
@@ -12,31 +12,41 @@ import { routes } from '../../../app.routes';
   standalone: true,
   imports: [MenubarModule, NgIf],
   templateUrl: './filter-header.component.html',
-  styleUrl: './filter-header.component.css'
+  styleUrl: './filter-header.component.css',
 })
 export class FilterHeaderComponent implements OnInit {
-//Services
-categoryService = inject(CategoryService);
+  //Services
+  categoryService = inject(CategoryService);
 
-//Variables
-categories = signal<Category[]>([]);
-items  = computed(() =>
-  this.categories().map(category => ({
+  //Variables
+  categories = signal<Category[]>([]);
+  items = computed(() =>
+    this.categories().map((category) => ({
       label: category.name,
       items: [
-      { label: 'All', route: 'courses/' + (category.name).toLowerCase() + '/all'}, 
-      ...category.subcategories.map(subcategory => ({
-        label: subcategory.name,
-        route: 'courses/'+ (category.name).toLowerCase() +'/'+   (subcategory.name).toLowerCase()
-      }))
-    ]
-  })));
+        {
+          label: 'All',
+          route: 'courses/' + category.name.toLowerCase() + '/all',
+        },
+        ...category.subcategories.map((subcategory) => ({
+          label: subcategory.name,
+          route:
+            'courses/' +
+            category.name.toLowerCase() +
+            '/' +
+            subcategory.name.toLowerCase(),
+        })),
+      ],
+    }))
+  );
 
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe((data) => {
+      this.categories.set(data);
+    });
+  }
 
-ngOnInit(): void {
-  this.categoryService.getCategories().subscribe((data) => {
-    this.categories.set(data);
-  });
-}
-
+  hideSubMenu() {
+    window.document.body.click();
+  }
 }
