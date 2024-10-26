97cb10b0a367bdd90de2a93c3425052a8bc8fb15
MadziaSzmatloch <magdaszmatloch@gmail.com>
2024-10-26 11:14:29 +0200

    display full course name on hoover

--- src/frontend/Skillup/src/app/course/components/course-item/course-item.component.ts
+++ src/frontend/Skillup/src/app/course/components/course-item/course-item.component.ts
@@ -2,13 +2,15 @@ import { Component, Input, input } from '@angular/core';
 import { CourseListItem } from '../../models/course.model';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { TooltipModule } from 'primeng/tooltip';
+import { TruncatePipe } from "../../../utils/pipes/truncate.pipe";
 
 @Component({
   selector: 'app-course-item',
   standalone: true,
-  imports: [ButtonModule, CardModule],
+  imports: [ButtonModule, CardModule, TruncatePipe, TooltipModule],
   templateUrl: './course-item.component.html',
-  styles: []
+  styleUrl: './course-item.component.css',
 })
 export class CourseItemComponent {
   @Input() course!: CourseListItem;
