dd0b612e1ec458bc93d14ff56175a2d82ee936b6
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-27 23:39:54 +0100

    code restructuring

--- src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Application/Features/Commands/Elements/AddArticleHandler.cs
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Application/Features/Commands/Elements/AddArticleHandler.cs
@@ -2,9 +2,9 @@
 using Skillup.Modules.Courses.Core.Entities.CourseEntities.CourseContent;
 using Skillup.Modules.Courses.Core.Entities.CourseEntities.CourseContent.Assets;
 using Skillup.Modules.Courses.Core.Interfaces;
-using Skillup.Modules.Courses.Core.Requests.Commands;
+using Skillup.Modules.Courses.Core.Requests.Commands.Elements;
 
-namespace Skillup.Modules.Courses.Application.Features.Commands
+namespace Skillup.Modules.Courses.Application.Features.Commands.Elements
 {
     public class AddArticleHandler : IRequestHandler<AddArticleRequest>
     {
