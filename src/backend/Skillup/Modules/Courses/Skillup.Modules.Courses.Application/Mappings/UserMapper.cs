03819f44b340057faffa75f2386d7f76cacede1c
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-28 19:59:42 +0100

    add fetching profile picture from S3 in UserDTO

--- src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Application/Mappings/UserMapper.cs
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Application/Mappings/UserMapper.cs
@@ -1,14 +1,18 @@
 ﻿using Riok.Mapperly.Abstractions;
 using Skillup.Modules.Courses.Core.DTO.User;
 using Skillup.Modules.Courses.Core.Entities.UserEntities;
+using Skillup.Shared.Abstractions.S3;
 
 namespace Skillup.Modules.Courses.Application.Mappings
 {
     [Mapper]
-    public partial class UserMapper
+    public partial class UserMapper(IAmazonS3Service s3Service)
     {
-        public UserDto UserToUserDto(User user, bool details)
+        private readonly IAmazonS3Service _s3Service = s3Service;
+
+        public async Task<UserDto> UserToUserDto(User user, bool details)
         {
+            var profilePicture = await _s3Service.GetPresignedUrl(S3FolderPaths.UserProfilePicture + user.ProfilePictureKey);
             if (details)
             {
                 return new DetailedUserDto()
@@ -17,7 +21,7 @@ namespace Skillup.Modules.Courses.Application.Mappings
                     FirstName = user.FirstName,
                     LastName = user.LastName,
                     Email = user.Email,
-                    ProfilePicture = user.ProfilePicture,
+                    ProfilePicture = new Uri(profilePicture),
                     Details = user.Details,
                     SocialMediaLinks = user.SocialMediaLinks,
                     PrivacySettings = user.PrivacySettings,
@@ -31,7 +35,7 @@ namespace Skillup.Modules.Courses.Application.Mappings
                     FirstName = user.FirstName,
                     LastName = user.LastName,
                     Email = user.Email,
-                    ProfilePicture = user.ProfilePicture,
+                    ProfilePicture = new Uri(profilePicture),
                 };
             }
         }
