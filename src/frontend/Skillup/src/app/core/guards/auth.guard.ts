27547d4ae494472bf9e7dbe2b0cf5c53d10c367b
MadziaSzmatloch <magdaszmatloch@gmail.com>
2024-10-28 21:14:07 +0100

    refresh 401 fix

--- src/frontend/Skillup/src/app/core/guards/auth.guard.ts
+++ src/frontend/Skillup/src/app/core/guards/auth.guard.ts
@@ -4,12 +4,16 @@ import { UserService } from "../../user/services/user.service";
 import { User } from "../../user/models/user.model";
 import { UserRole } from "../../user/models/user-role.model";
 import { firstValueFrom, take } from "rxjs";
+import { AuthService } from "../../auth/services/auth.service";
 
 export const hasRole: CanMatchFn = async (route, segments) => {
     const router = inject(Router);
     const userService = inject(UserService)
+    const authService = inject(AuthService)
     const user = signal<User | null>(null);
     const requiredRole = route.data?.['requiredRole'];
+    //authService.autoSignIn();
+    await new Promise(resolve => setTimeout(resolve, 1000)); 
     userService.user.subscribe((data) => {
         next: {
             user.set(data);
