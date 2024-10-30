d18a99a79548e8451e7d71b4ca7750bb17cf9430
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-30 23:26:15 +0100

    hero section styling

--- src/frontend/Skillup/src/app/core/components/hero/hero.component.ts
+++ src/frontend/Skillup/src/app/core/components/hero/hero.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CourseCarouselComponent } from '../../../course/components/courses-carousels/course-carousel/course-carousel.component';
+import { AuthService } from '../../../auth/services/auth.service';
+import { CoursesService } from '../../../course/services/course.service';
+import { CategoryService } from '../../../course/services/category.service';
+import { CourseListItem } from '../../../course/models/course.model';
+import { Category } from '../../../course/models/category.model';
 
 @Component({
   selector: 'app-hero',
@@ -11,5 +16,48 @@ import { CourseCarouselComponent } from '../../../course/components/courses-caro
   styleUrl: './hero.component.css',
 })
 export class HeroComponent implements OnInit {
-  ngOnInit(): void {}
+  coursesService = inject(CoursesService);
+  cetegoriesService = inject(CategoryService);
+
+  authService = inject(AuthService);
+  route = inject(ActivatedRoute);
+
+  userId: string | null = null;
+  activationToken: string | null = null;
+
+  selectedCategories = signal<Category[]>([]);
+
+  courseCaruseles = computed(() => {
+    console.log(this.selectedCategories());
+
+    return this.selectedCategories().map((x) =>
+      this.coursesService.getCouresByCategoryId(x.id)
+    );
+  });
+
+  ngOnInit(): void {
+    this.userId = this.route.snapshot.queryParamMap.get('userId');
+    this.activationToken =
+      this.route.snapshot.queryParamMap.get('activationToken');
+
+    const categories = this.cetegoriesService.categories();
+
+    this.selectedCategories.set([
+      ...new Set(
+        Array.from(
+          { length: 3 },
+          () => categories[Math.floor(Math.random() * categories.length)]
+        )
+      ),
+    ]);
+
+    this.activateAccount();
+  }
+
+  activateAccount() {
+    if (this.userId !== null && this.activationToken !== null)
+      this.authService
+        .activateAccount(this.userId, this.activationToken)
+        .subscribe();
+  }
 }
