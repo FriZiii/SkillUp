209323c4719139a9764f5e5c0d731d4c970a3847
MadziaSzmatloch <magdaszmatloch@gmail.com>
2024-10-30 22:57:09 +0100

    Update errorhandler.service.ts

--- src/frontend/Skillup/src/app/core/services/errorhandler.service.ts
+++ src/frontend/Skillup/src/app/core/services/errorhandler.service.ts
@@ -14,8 +14,14 @@ export class ErrorHandlerService implements ErrorHandler {
 
   handleError(error: any): void {
     console.error('Error occurred:', error);
-    if (error && error.message && !error.message.includes('offsetWidth')) {
+    if (error.status === 502) {
+      this.toastService.showError('Api not working');
+    }
+    if (error.error.errors[0].code) {
+      this.toastService.showError(error.error.errors[0].message);
+    }
+    else if (error && error.message && !error.message.includes('offsetWidth')) {
       this.toastService.showError(error.message);
-  }
+    }
   }
 }
