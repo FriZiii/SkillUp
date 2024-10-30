0dbde2a548ff2348c044acfe090cfcf9ebc163bd
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-10-30 19:07:56 +0100

    hero init

--- src/frontend/Skillup/src/app/core/components/hero/hero.component.ts
+++ src/frontend/Skillup/src/app/core/components/hero/hero.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { CourseCarouselComponent } from '../../../course/components/courses-carousels/course-carousel/course-carousel.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [ButtonModule, RouterLink],
+  imports: [ButtonModule, RouterLink, CourseCarouselComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
-export class HeroComponent {}
+export class HeroComponent implements OnInit {
+  ngOnInit(): void {}
+}
