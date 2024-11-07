ffb0c887978d5c79a87554258426fa728a3ea022
MadziaSzmatloch <98941475+MadziaSzmatloch@users.noreply.github.com>
2024-11-07 11:02:33 +0100

    Update GetCoursesHandler.cs

--- src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Application/Features/Queries/GetCoursesHandler.cs
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Application/Features/Queries/GetCoursesHandler.cs
@@ -10,17 +10,14 @@ namespace Skillup.Modules.Courses.Application.Features.Queries
     public class GetCoursesHandler : IRequestHandler<GetCoursesRequest, IEnumerable<CourseDto>>
     {
         private readonly ICourseRepository _courseRepository;
-        private readonly ILogger<GetCoursesHandler> _logger;
 
-        public GetCoursesHandler(ICourseRepository courseRepository, ILogger<GetCoursesHandler> logger)
+        public GetCoursesHandler(ICourseRepository courseRepository)
         {
             _courseRepository = courseRepository;
-            _logger = logger;
         }
 
         public async Task<IEnumerable<CourseDto>> Handle(GetCoursesRequest request, CancellationToken cancellationToken)
         {
-            _logger.LogInformation("Get Course handler");
             var mapper = new CourseMapper();
             var courses = await _courseRepository.GetAll();
             var coursesDtos = courses.Select(mapper.CourseToCourseDto).ToList();
