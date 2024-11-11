a77407ad87b0634cf92d784b09b9167b69ab19a6
MadziaSzmatloch <magdaszmatloch@gmail.com>
2024-11-11 17:02:15 +0100

    update do deleting

--- src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Infrastracture/Repositories/ElementRepository.cs
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Infrastracture/Repositories/ElementRepository.cs
@@ -56,15 +56,10 @@ namespace Skillup.Modules.Courses.Infrastracture.Repositories
         {
             var element = _elements.FirstOrDefault(e => e.Id == elementId) ?? throw new Exception();
             _elements.Remove(element);
-            var elementsToChange = await _elements.Where(e => e.SectionId == element.SectionId && e.Id != element.Id).OrderBy(x => x.Index)
-                .ToListAsync();
+            var elementsToChange = _elements.Where(e => e.SectionId == element.SectionId && e.Id != element.Id).OrderBy(x => x.Index);
             for (int i = 0; i < elementsToChange.Count(); i++)
             {
-                elementsToChange[i].Index = i;
-            }
-            foreach (var tempElement in _elements.Where(s => elementsToChange.Select(se => se.Id).Contains(s.Id)))
-            {
-                tempElement.Index = elementsToChange.First(el => el.Id == tempElement.Id).Index;
+                elementsToChange.ElementAt(i).Index = i;
             }
             await _context.SaveChangesAsync();
         }
