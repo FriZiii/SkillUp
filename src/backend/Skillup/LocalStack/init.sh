1ed6c7f28369f5c3ff95218c9203b06e26caedd9
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-11-13 16:21:58 +0100

    added localstack cors

--- src/backend/Skillup/LocalStack/init.sh
+++ src/backend/Skillup/LocalStack/init.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 awslocal s3 mb s3://skillup-s3-dev
+awslocal s3api put-bucket-cors --bucket skillup-s3-dev --cors-configuration file:///etc/localstack/init/ready.d/cors-config.json
