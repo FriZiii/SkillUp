b9abb022d619d141663daf301212867357f94171
MadziaSzmatloch <magdaszmatloch@gmail.com>
2024-11-29 18:28:18 +0100

    isAuthor gouard, invalid credentials added to error handler

--- src/frontend/Skillup/src/app/app.routes.ts
+++ src/frontend/Skillup/src/app/app.routes.ts
@@ -13,7 +13,7 @@ import { CourseDetailComponent } from './course/components/course-detail/course-
 import { NotFoundComponent } from './core/components/not-found/not-found.component';
 import { AccessDeniedComponent } from './core/components/access-denied/access-denied.component';
 import { UserRole } from './user/models/user-role.model';
-import { hasRole } from './core/guards/auth.guard';
+import { hasRole, isAuthor } from './core/guards/auth.guard';
 import { OtherUserProfileComponent } from './user/components/other-user-profile/other-user-profile.component';
 import { EditCourseComponent } from './course/components/edit-course/edit-course.component';
 import { CourseCreatorComponent } from './course/components/edit-course/course-creator/course-creator.component';
@@ -54,6 +54,7 @@ export const routes: Routes = [
   {
     path: 'course-edit/:courseId',
     component: EditCourseComponent,
+    canMatch: [isAuthor],
     children: [
       { path: 'creator', component: CourseCreatorComponent },
       { path: 'essentials', component: CourseEssentialsComponent },
--- src/frontend/Skillup/src/app/core/guards/auth.guard.ts
+++ src/frontend/Skillup/src/app/core/guards/auth.guard.ts
@@ -1,7 +1,10 @@
-import { inject, signal } from "@angular/core"
+import { computed, inject, signal } from "@angular/core"
 import { CanMatchFn, Router } from "@angular/router"
 import { UserService } from "../../user/services/user.service";
 import { User } from "../../user/models/user.model";
+import { UserRole } from "../../user/models/user-role.model";
+import { CoursesService } from "../../course/services/course.service";
+import { CourseListItem } from "../../course/models/course.model";
 
 export const hasRole: CanMatchFn = async (route, segments) => {
     const router = inject(Router);
@@ -18,6 +21,29 @@ export const hasRole: CanMatchFn = async (route, segments) => {
     if(user()?.isInRole(requiredRole)){
         return true;
     }
+    router.navigate(['/access-denied']);
+    return false;
+}
+
+export const isAuthor: CanMatchFn = async (route, segments) => {
+    console.log('lol');
+    const router = inject(Router);
+    const userService = inject(UserService)
+    const courseSerivce = inject(CoursesService)
+    const user = signal<User | null>(null);
+    const courseId = segments[1].path;
+    const course = computed(() => courseSerivce.courses().find(c => c.id === courseId))
+    await new Promise(resolve => setTimeout(resolve, 1000)); 
+    userService.user.subscribe((data) => {
+        next: {
+            user.set(data);
+        }
+    })
+    
+    if(user()?.isInRole(UserRole.Instructor) && user()?.id === course()?.authorId){
+        return true;
+    }
+    
     router.navigate(['/access-denied']);
     return false;
 }
