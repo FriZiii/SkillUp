fed6fb189dd0951244fb5e4c17adc3b96abaa5ba
MadziaSzmatloch <magdaszmatloch@gmail.com>
2024-12-03 19:04:45 +0100

    styling of author courses

--- src/frontend/Skillup/src/app/course/components/courses-created-by-you/courses-created-by-you.component.ts
+++ src/frontend/Skillup/src/app/course/components/courses-created-by-you/courses-created-by-you.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { CoursesService } from '../../services/course.service';
 import { Course } from '../../models/course.model';
 import { CourseItemShortComponent } from "../course-item-short/course-item-short.component";
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-courses-created-by-you',
   standalone: true,
-  imports: [CourseItemShortComponent],
+  imports: [CourseItemShortComponent, ProgressSpinnerModule],
   templateUrl: './courses-created-by-you.component.html',
   styleUrl: './courses-created-by-you.component.css'
 })
@@ -14,6 +15,7 @@ export class CoursesCreatedByYouComponent implements OnInit {
   //Variables
   authorId = input<string>();
   courses = signal<Course[]>([]);
+  loading = true;
 
   //Services
   courseService = inject(CoursesService);
@@ -24,6 +26,7 @@ export class CoursesCreatedByYouComponent implements OnInit {
       (res) => {
         this.courses.set(res);
         this.courses.set(this.courses().slice(0, 30));
+        this.loading = false;
       }
     )
   }
