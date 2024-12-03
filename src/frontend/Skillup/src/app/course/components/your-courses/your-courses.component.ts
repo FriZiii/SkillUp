23f3433477058819123de2f66563e10c18426d30
MadziaSzmatloch <magdaszmatloch@gmail.com>
2024-12-03 19:26:41 +0100

    more things added

--- src/frontend/Skillup/src/app/course/components/your-courses/your-courses.component.ts
+++ src/frontend/Skillup/src/app/course/components/your-courses/your-courses.component.ts
@@ -1,12 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit, signal } from '@angular/core';
+import { Course } from '../../models/course.model';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { CourseItemShortComponent } from "../course-item-short/course-item-short.component";
 
 @Component({
   selector: 'app-your-courses',
   standalone: true,
-  imports: [],
+  imports: [ProgressSpinnerModule, CourseItemShortComponent],
   templateUrl: './your-courses.component.html',
   styleUrl: './your-courses.component.css'
 })
-export class YourCoursesComponent {
+export class YourCoursesComponent implements OnInit {
   userId = input<string>();
+  loading = true;
+  courses = signal<Course[]>([]);
+  
+  ngOnInit(): void {
+    this.loading = false;
+  }
 }
