7053d51c9c7e210530ad0874d7b1250443c2211d
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-12-04 19:29:09 +0100

    purchased history has been removed

--- src/backend/Skillup/Modules/Finances/Skillup.Modules.Finances.Core/DAL/FinancesDbContext.cs
+++ src/backend/Skillup/Modules/Finances/Skillup.Modules.Finances.Core/DAL/FinancesDbContext.cs
@@ -8,7 +8,6 @@ namespace Skillup.Modules.Finances.Core.DAL
         public DbSet<User> Users { get; set; }
         public DbSet<Wallet> Wallets { get; set; }
         public DbSet<Item> Items { get; set; }
-        public DbSet<PurchaseHistory> PurchaseHistories { get; set; }
 
         public DbSet<DiscountCode> DiscountCodes { get; set; }
         public DbSet<DiscountedItem> DiscountedItems { get; set; }
