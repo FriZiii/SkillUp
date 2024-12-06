a116b14c3860656d4d10fda4992496925c764dea
MadziaSzmatloch <magdaszmatloch@gmail.com>
2024-12-06 18:56:50 +0100

    buy currncy styling

--- src/frontend/Skillup/src/app/finance/components/buy-currency/buy-currency.component.ts
+++ src/frontend/Skillup/src/app/finance/components/buy-currency/buy-currency.component.ts
@@ -1,28 +1,50 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputMaskModule } from 'primeng/inputmask';
 import { ButtonModule } from 'primeng/button';
 import { WalletService } from '../../services/wallet.service';
+import { CommonModule } from '@angular/common';
+import { tap } from 'rxjs';
+import { ToastHandlerService } from '../../../core/services/toast-handler.service';
+import { ConfirmationDialogHandlerService } from '../../../core/services/confirmation-handler.service';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-buy-currency',
   standalone: true,
-  imports: [InputTextModule, FormsModule, InputMaskModule, ButtonModule],
+  imports: [InputTextModule, ReactiveFormsModule, InputMaskModule, ButtonModule, CommonModule, ConfirmDialogModule],
   templateUrl: './buy-currency.component.html',
   styleUrl: './buy-currency.component.css'
 })
 export class BuyCurrencyComponent {
   //Services
   walletService = inject(WalletService);
+  toastService = inject(ToastHandlerService);
+  confirmationDialogService = inject(ConfirmationDialogHandlerService);
 
   //Variables
-  cardNumber = 0;
-  expirationDate = '';
-  CVC = 0;
-  ownerName = '';
+  cardform = new FormGroup({
+    cardNumber: new FormControl('', {
+      validators: [Validators.minLength(16), Validators.required],
+    }),
+    expirationDate: new FormControl('', {
+      validators: [Validators.minLength(4), Validators.required],
+    }),
+    CVC: new FormControl('', {
+      validators: [Validators.minLength(3), Validators.required],
+    }),
+    ownerName: new FormControl('', {
+      validators: [Validators.required],
+    })
+  });
 
-  purchaseCurrency(event: Event){
-    this.walletService.addBalance(100).subscribe();
+  purchaseCurrency(event: Event, balance: number){
+    this.confirmationDialogService.confirmSave(event, () => {
+      this.walletService.addBalance(balance).pipe(
+        tap((res) => {
+          this.toastService.showSuccess('Successfully added: ' + balance + ' $');
+        })).subscribe();
+    })
   }
 }
