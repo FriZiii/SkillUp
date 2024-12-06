a38f16f2fc060cf001535907abde2a863633f866
MadziaSzmatloch <magdaszmatloch@gmail.com>
2024-12-06 19:20:12 +0100

    order summary styling

--- src/frontend/Skillup/src/app/finance/components/cart/cart.component.ts
+++ src/frontend/Skillup/src/app/finance/components/cart/cart.component.ts
@@ -55,7 +55,7 @@ export class CartComponent {
         this.invalidCode = true;
       },
     }
-    );
+    ); 
   }
 
   delCode(){
--- src/frontend/Skillup/src/app/finance/components/order-summary/order-summary.component.ts
+++ src/frontend/Skillup/src/app/finance/components/order-summary/order-summary.component.ts
@@ -3,11 +3,13 @@ import { CartService } from '../../services/cart.service';
 import { ButtonModule } from 'primeng/button';
 import { WalletService } from '../../services/wallet.service';
 import { RouterModule } from '@angular/router';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationDialogHandlerService } from '../../../core/services/confirmation-handler.service';
 
 @Component({
   selector: 'app-order-summary',
   standalone: true,
-  imports: [ButtonModule, RouterModule],
+  imports: [ButtonModule, RouterModule, ConfirmDialogModule],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.css'
 })
@@ -15,14 +17,15 @@ export class OrderSummaryComponent {
   //Services
   cartService = inject(CartService);
   walletService = inject(WalletService);
+  confirmationDialogService = inject(ConfirmationDialogHandlerService);
 
   //Variables
   cart = this.cartService.cart;
   wallet = this.walletService.currentWallet;
 
   purchaseCart(event: Event){
-    console.log(this.wallet())
-    console.log(this.cart())
-//this.cartService.checkoutCart().subscribe();
+    this.confirmationDialogService.confirmSave(event, () => {
+      this.cartService.checkoutCart().subscribe();
+    })
   }
 }
