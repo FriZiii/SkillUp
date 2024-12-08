d142bb0739dc7bfbf1a58e75ed76471cb8636779
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-12-08 09:24:55 +0100

    Update discountCode.service.ts

--- src/frontend/Skillup/src/app/finance/services/discountCode.service.ts
+++ src/frontend/Skillup/src/app/finance/services/discountCode.service.ts
@@ -1,29 +1,42 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-import { environment } from "../../../environments/environment";
-import { AddDiscountCode, DiscountCode, DiscountCodeType } from "../models/discountCodes.model";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import {
+  AddDiscountCode,
+  DiscountCode,
+  DiscountCodeType,
+} from '../models/discountCodes.model';
 
 @Injectable({ providedIn: 'root' })
 export class DiscountCodeService {
   private httpClient = inject(HttpClient);
 
-  public addDiscountCode(type: DiscountCodeType, addDiscountCode: AddDiscountCode){
-    return this.httpClient
-      .post<DiscountCode>(environment.apiUrl + '/Finances/DiscountCode/' + type , {addDiscountCode});
+  public addDiscountCode(
+    type: DiscountCodeType,
+    addDiscountCode: AddDiscountCode
+  ) {
+    return this.httpClient.post<DiscountCode>(
+      environment.apiUrl + '/Finances/DiscountCode/' + type,
+      addDiscountCode
+    );
   }
 
-  public getDiscountCodesByOwner(userId: string){
-    return this.httpClient
-      .get<DiscountCode[]>(environment.apiUrl + '/Finances/DiscountCode/' + userId);
+  public getDiscountCodesByOwner(userId: string) {
+    return this.httpClient.get<DiscountCode[]>(
+      environment.apiUrl + '/Finances/DiscountCode/' + userId
+    );
   }
 
-  public editDiscountCode(codeId: string, addDiscountCode: AddDiscountCode){
-    return this.httpClient
-      .put<DiscountCode>(environment.apiUrl + '/Finances/DiscountCode/' + codeId, {addDiscountCode});
+  public editDiscountCode(codeId: string, addDiscountCode: AddDiscountCode) {
+    return this.httpClient.put<DiscountCode>(
+      environment.apiUrl + '/Finances/DiscountCode/' + codeId,
+      addDiscountCode
+    );
   }
 
-  public deleteDiscountCode(codeId: string){
-    return this.httpClient
-      .delete(environment.apiUrl + '/Finances/DiscountCode/' + codeId);
+  public deleteDiscountCode(codeId: string) {
+    return this.httpClient.delete(
+      environment.apiUrl + '/Finances/DiscountCode/' + codeId
+    );
   }
 }
