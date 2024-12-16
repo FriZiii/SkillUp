86e52c74ac5a9082be1b16002883893a28bcbc56
MadziaSzmatloch <magdaszmatloch@gmail.com>
2024-12-16 21:08:16 +0100

    cart update

--- src/frontend/Skillup/src/app/finance/components/cart-item/cart-item.component.ts
+++ src/frontend/Skillup/src/app/finance/components/cart-item/cart-item.component.ts
@@ -3,11 +3,14 @@ import { CartItemForDisplay } from '../../models/cart.model';
 import { RouterModule } from '@angular/router';
 import { TruncatePipe } from "../../../utils/pipes/truncate.pipe";
 import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart-item',
   standalone: true,
-  imports: [RouterModule, TruncatePipe, ButtonModule],
+  imports: [RouterModule, TruncatePipe, ButtonModule, CommonModule, RatingModule, FormsModule],
   templateUrl: './cart-item.component.html',
   styleUrl: './cart-item.component.css'
 })
