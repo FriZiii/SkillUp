3cc2732a0d7041c97e707f3c16e1bb90b17b0dc9
MadziaSzmatloch <magdaszmatloch@gmail.com>
2024-12-20 08:20:59 +0100

    category filtering

--- src/frontend/Skillup/src/app/course/components/filter-for-courses/filter-for-courses.component.ts
+++ src/frontend/Skillup/src/app/course/components/filter-for-courses/filter-for-courses.component.ts
@@ -1,26 +1,66 @@
-import { Component, input, OnChanges, output, SimpleChanges } from '@angular/core';
+import { Component, computed, inject, input, OnChanges, output, signal, SimpleChanges } from '@angular/core';
 import { CourseListItem } from '../../models/course.model';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../models/category.model';
 
 @Component({
   selector: 'app-filter-for-courses',
   standalone: true,
-  imports: [InputTextModule, FormsModule],
+  imports: [InputTextModule, FormsModule, SelectModule],
   templateUrl: './filter-for-courses.component.html',
   styleUrl: './filter-for-courses.component.css'
 })
 export class FilterForCoursesComponent implements OnChanges {
   courses = input.required<CourseListItem[]>();
   filteredCourses = output<CourseListItem[]>();
+  defaultCategory = input<string>('');
+  defaultSubcategory = input<string>('');
 
+  //Services
+  courseCategoryService = inject(CategoryService)
+
+  //Variables
   title = '';
   authorName = '';
+  selectedCategory = signal('');
+  selectedSubcategory = signal('');
+
+  //Selects
+  categories = this.courseCategoryService.categories;
+  categoryNames = computed(() =>
+    this.categories().map((category) => ({
+      id: category.id,
+      name: category.name,
+    }))
+  );
+  subcategoryNames = computed(() => {
+    var selectedCat : Category | undefined;
+    if(this.defaultCategory() !== ''){
+      const cat = this.categories().find(c => c.slug === this.defaultCategory());
+      selectedCat = this.categories().find(
+        (category) => category.id === cat?.id
+      );
+    }
+    else{
+      selectedCat = this.categories().find(
+        (category) => category.id === this.selectedCategory()
+      );
+    }
+    return selectedCat
+      ? selectedCat.subcategories.map((sub) => ({ id: sub.id, name: sub.name }))
+      : [];
+  });
+
 
   ngOnChanges(changes: SimpleChanges): void {
     if(changes['courses']){
       this.title = '';
       this.authorName = '';
+      this.selectedCategory.set('')
+      this.selectedSubcategory.set('')
     }
   }
 
@@ -29,7 +69,9 @@ export class FilterForCoursesComponent implements OnChanges {
       .filter(course => {
         const matchesSearch = course.title?.toLowerCase().includes(this.title.toLowerCase());
         const matchesAuthor = course.authorName?.toLowerCase().includes(this.authorName.toLowerCase());
-        return matchesSearch && matchesAuthor;
+        const matchesCategory = course.category?.id.match(this.selectedCategory());
+        const matchesSubcategory = course.category?.subcategory.id.includes(this.selectedSubcategory());
+        return matchesSearch && matchesAuthor && matchesCategory && matchesSubcategory;
       });
 
     this.filteredCourses.emit(filtered);
