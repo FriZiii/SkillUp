bdb1d559a842c63ef36aedbe5a2188e6f827dbab
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-12-20 22:26:35 +0100

    attachments endpoints refactor

---
+++ src/backend/Skillup/Modules/Courses/Skillup.Modules.Courses.Application/Features/Queries/GetAttachmentsByCourseIdHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Skillup.Modules.Courses.Application.Mappings;
+using Skillup.Modules.Courses.Core.DTO;
+using Skillup.Modules.Courses.Core.Interfaces;
+using Skillup.Modules.Courses.Core.Requests.Queries;
+
+namespace Skillup.Modules.Courses.Application.Features.Queries
+{
+    internal class GetAttachmentsByCourseIdHandler(IElementAttachmentRepository elementAttachmentRepository) : IRequestHandler<GetAttachmentsByCourseIdRequest, IEnumerable<AttachmentDto>>
+    {
+        private readonly IElementAttachmentRepository _elementAttachmentRepository = elementAttachmentRepository;
+
+        public async Task<IEnumerable<AttachmentDto>> Handle(GetAttachmentsByCourseIdRequest request, CancellationToken cancellationToken)
+        {
+            var mapper = new AttachmentsMapper();
+            var attachments = await _elementAttachmentRepository.GetByCourseId(request.CourseId);
+            return attachments.Select(mapper.AttachmentToDto);
+        }
+    }
+}
