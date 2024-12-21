412cfd3d64a6a15c3420e62a06d148bcb87463f1
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-12-21 19:21:18 +0100

    added start and expire at date to discount code

--- src/backend/Skillup/Modules/Finances/Skillup.Modules.Finances.Core/DAL/Migrations/20241221182032_Add_Start_And_Expires_Date_To_Discount_Code.Designer.cs
+++ src/backend/Skillup/Modules/Finances/Skillup.Modules.Finances.Core/DAL/Migrations/20241221182032_Add_Start_And_Expires_Date_To_Discount_Code.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Skillup.Modules.Finances.Core.DAL;
@@ -11,9 +12,11 @@ using Skillup.Modules.Finances.Core.DAL;
 namespace Skillup.Modules.Finances.Core.DAL.Migrations
 {
     [DbContext(typeof(FinancesDbContext))]
-    partial class FinancesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241221182032_Add_Start_And_Expires_Date_To_Discount_Code")]
+    partial class Add_Start_And_Expires_Date_To_Discount_Code
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/backend/Skillup/Modules/Finances/Skillup.Modules.Finances.Core/DAL/Migrations/20241221182032_Add_Start_And_Expires_Date_To_Discount_Code.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Skillup.Modules.Finances.Core.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Start_And_Expires_Date_To_Discount_Code : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpireAt",
+                schema: "finances",
+                table: "DiscountCodes",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartAt",
+                schema: "finances",
+                table: "DiscountCodes",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpireAt",
+                schema: "finances",
+                table: "DiscountCodes");
+
+            migrationBuilder.DropColumn(
+                name: "StartAt",
+                schema: "finances",
+                table: "DiscountCodes");
+        }
+    }
+}
