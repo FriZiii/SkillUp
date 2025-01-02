0fe54d610d4892013336c2a97c3224b02e352b83
MadziaSzmatloch <magdaszmatloch@gmail.com>
2025-01-02 20:22:31 +0100

    update to mini code

--- src/frontend/Skillup/src/app/finance/components/mini-code/mini-code.component.ts
+++ src/frontend/Skillup/src/app/finance/components/mini-code/mini-code.component.ts
@@ -1,15 +1,17 @@
 import { Component, input } from '@angular/core';
 import { DiscountCode, DiscountCodeType } from '../../models/discountCodes.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mini-code',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mini-code.component.html',
   styleUrl: './mini-code.component.css'
 })
 export class MiniCodeComponent {
   code = input.required<DiscountCode>();
+  copied = false;
 
   getType(){
     if(this.code().type === DiscountCodeType.Percentage){
@@ -21,6 +23,11 @@ export class MiniCodeComponent {
   }
 
   copyCode(){
+    this.copied = true;
     navigator.clipboard.writeText(this.code().code);
+
+    setTimeout(() => {
+      this.copied = false;
+    }, 2000);
   }
 }
